c44327ae846020d54ee472d0c89e734c352fb518
sohailpasha1 <157475077+sohailpasha1@users.noreply.github.com>
2024-02-05 14:43:19 +0530

    Create DrawImageToPDF.java

---
+++ src/document/pdf/create/DrawImageToPDF.java
@@ -0,0 +1,37 @@
+public class DrawImageToPDF{
+/**
+     * Draw Image into a individual PDF File
+     */
+    public void drawImageToPDF(List<File> fileList) throws IOException {
+        for (File file : fileList) {
+            try (PDDocument pdDocument = new PDDocument()) {
+                this.drawingImageAsNewPage(pdDocument, file.toPath().toString());
+                pdDocument.save("D://Location" + file.getName());
+            }
+        }
+    }
+
+    /**
+     * this method converts image to pdf
+     */
+    public void drawingImageAsNewPage(PDDocument doc, String imagePath) throws IOException {
+        PDImageXObject image = PDImageXObject.createFromFile(imagePath, doc);
+        PDRectangle pageSize = PDRectangle.A4;
+        int originalWidth = image.getWidth();
+        int originalHeight = image.getHeight();
+        float pageWidth = pageSize.getWidth();
+        float pageHeight = pageSize.getHeight();
+        float ratio = Math.min(pageWidth / originalWidth, pageHeight / originalHeight);
+        float scaledWidth = originalWidth * ratio;
+        float scaledHeight = originalHeight * ratio;
+        float x = (pageWidth - scaledWidth) / 2;
+        float y = (pageHeight - scaledHeight) / 2;
+        PDPage page = new PDPage(pageSize);
+        doc.addPage(page);
+        try (PDPageContentStream contents = new PDPageContentStream(doc, page)) {
+            contents.drawImage(image, x, y, scaledWidth, scaledHeight);
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+}
